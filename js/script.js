b8ec60447a7285bac89f4ad589efe2839567255f
TrungMai <anh.maitrung@gmail.com>
2022-10-29 13:22:22 +0700

    fix bug

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ document.getElementById("btnDoiCho").onclick = doiCho;
 
 function tangDan(){
     for(var i=0;i<array.length-1;i++){
-        for(j=0; j<array.length; j++){
+        for(var j=0; j<array.length; j++){
             if(array[j]>array[j+1]){
                 array[j] = array[j] + array[j+1];
                 array[j+1] = array[j] - array[j+1];
