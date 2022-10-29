26148f0234db36304d90093f74ed9468b983a56f
TrungMai <anh.maitrung@gmail.com>
2022-10-29 14:15:30 +0700

    refactor

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ document.getElementById("btnDoiCho").onclick = doiCho;
 
 function tangDan(){
     for(var i=0;i<array.length-1;i++){
-        for(var j=0; j<array.length; j++){
+        for(var j=0; j<array.length-i; j++){
             if(array[j]>array[j+1]){
                 array[j] = array[j] + array[j+1];
                 array[j+1] = array[j] - array[j+1];
